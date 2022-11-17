5cd0567840c1a30e6df9be555f4728e794b12bf4
enoblivioner <xucao560@163.com>
2022-11-18 00:36:29 +0800

    尝试解决IP SAN的问题

--- internal/testdata/generate_key.sh
+++ internal/testdata/generate_key.sh
@@ -5,7 +5,7 @@ set -e
 echo "Generating CA key and certificate:"
 openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:2048 \
   -keyout ca.key -out ca.pem \
-  -subj "/O=quic-go Certificate Authority/"
+  -subj "/C=CN/ST=BeiJing/L=HaiDian/CN=3unique.top"
 
 echo "Generating CSR"
 openssl req -out cert.csr -new -newkey rsa:2048 -nodes -keyout priv.key \
@@ -14,7 +14,7 @@ openssl req -out cert.csr -new -newkey rsa:2048 -nodes -keyout priv.key \
 echo "Sign certificate:"
 openssl x509 -req -sha256 -days 3650 -in cert.csr  -out cert.pem \
   -CA ca.pem -CAkey ca.key -CAcreateserial \
-  -extfile <(printf "subjectAltName=DNS:localhost")
+  -extfile <(printf "subjectAltName=DNS:3unique.top")
 
 # debug output the certificate
 openssl x509 -noout -text -in cert.pem
